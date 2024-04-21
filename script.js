c374d8d0aa2f9a1d0c4856eddad77b3568e61d32
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-21 21:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validEmail(str) {
-  //your JS code here.
+    var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(str);
 }
-
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
